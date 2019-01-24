3a6c79f7e5dacc792364b34c1b01dc4dd5dbeb17
Ola Jeppsson <ola.jeppsson@gmail.com>
2019-01-23 21:03:37 -0500

    machine: uart16550: Support Xilinx AXI UART16550
    Add "xlnx,xps-uart16550-2.00.a" to the compatible list.
    
    Check for these properties in the device tree node:
    "reg-offset", "reg-shift", "clock-frequency", "current-speed"
    
    Derive the clock divisor from the device tree, and do not hardcode the
    line speed to 38400 bauds.
    If the clock divisor cannot be derived from the device tree, leave it at
    its boot setting.

--- machine/uart16550.c
+++ machine/uart16550.c
@@ -5,22 +5,33 @@
 #include "fdt.h"
 
 volatile uint8_t* uart16550;
+uintptr_t uart16550_regshift;
 
 #define UART_REG_QUEUE     0
 #define UART_REG_LINESTAT  5
 #define UART_REG_STATUS_RX 0x01
 #define UART_REG_STATUS_TX 0x20
 
+static inline void uart16550_out(int reg, uint8_t val)
+{
+  uart16550[reg << uart16550_regshift] = val;
+}
+
+static inline uint8_t uart16550_in(int reg)
+{
+  return uart16550[reg << uart16550_regshift];
+}
+
 void uart16550_putchar(uint8_t ch)
 {
-  while ((uart16550[UART_REG_LINESTAT] & UART_REG_STATUS_TX) == 0);
-  uart16550[UART_REG_QUEUE] = ch;
+  while ((uart16550_in(UART_REG_LINESTAT) & UART_REG_STATUS_TX) == 0);
+  uart16550_out(UART_REG_QUEUE, ch);
 }
 
 int uart16550_getchar()
 {
-  if (uart16550[UART_REG_LINESTAT] & UART_REG_STATUS_RX)
-    return uart16550[UART_REG_QUEUE];
+  if (uart16550_in(UART_REG_LINESTAT) & UART_REG_STATUS_RX)
+    return uart16550_in(UART_REG_QUEUE);
   return -1;
 }
 
@@ -28,6 +39,9 @@ struct uart16550_scan
 {
   int compat;
   uint64_t reg;
+  uint32_t regshift;
+  uint32_t currentspeed;
+  uint32_t clockfrequency;
 };
 
 static void uart16550_open(const struct fdt_scan_node *node, void *extra)
@@ -38,27 +52,48 @@ static void uart16550_open(const struct fdt_scan_node *node, void *extra)
 
 static void uart16550_prop(const struct fdt_scan_prop *prop, void *extra)
 {
+  uint64_t reg;
   struct uart16550_scan *scan = (struct uart16550_scan *)extra;
+
   if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "ns16550a")) {
     scan->compat = 1;
+  } else if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "xlnx,xps-uart16550-2.00.a")) {
+    scan->compat = 1;
   } else if (!strcmp(prop->name, "reg")) {
-    fdt_get_address(prop->node->parent, prop->value, &scan->reg);
+    fdt_get_address(prop->node->parent, prop->value, &reg);
+    scan->reg += reg;
+  } else if (!strcmp(prop->name, "reg-offset")) {
+    scan->reg += bswap(*prop->value);
+  } else if (!strcmp(prop->name, "reg-shift")) {
+    scan->regshift = bswap(*prop->value);
+  } else if (!strcmp(prop->name, "current-speed")) {
+    scan->currentspeed = bswap(*prop->value);
+  } else if (!strcmp(prop->name, "clock-frequency")) {
+    scan->clockfrequency = bswap(*prop->value);
   }
 }
 
 static void uart16550_done(const struct fdt_scan_node *node, void *extra)
 {
   struct uart16550_scan *scan = (struct uart16550_scan *)extra;
+  uint16_t divisor = 0;
   if (!scan->compat || !scan->reg || uart16550) return;
 
   uart16550 = (void*)(uintptr_t)scan->reg;
+  uart16550_regshift = scan->regshift;
+  if (scan->clockfrequency && scan->currentspeed) {
+    divisor = scan->clockfrequency / (16 * scan->currentspeed);
+  }
+
   // http://wiki.osdev.org/Serial_Ports
-  uart16550[1] = 0x00;    // Disable all interrupts
-  uart16550[3] = 0x80;    // Enable DLAB (set baud rate divisor)
-  uart16550[0] = 0x03;    // Set divisor to 3 (lo byte) 38400 baud
-  uart16550[1] = 0x00;    //                  (hi byte)
-  uart16550[3] = 0x03;    // 8 bits, no parity, one stop bit
-  uart16550[2] = 0xC7;    // Enable FIFO, clear them, with 14-byte threshold
+  uart16550_out(1, 0x00); // Disable all interrupts
+  if (divisor) {
+    uart16550_out(3, 0x80); // Enable DLAB (set baud rate divisor)
+    uart16550_out(0, (divisor >> 0) & 0xff); // Set divisor (lo byte)
+    uart16550_out(1, (divisor >> 8) & 0xff); //             (hi byte)
+  }
+  uart16550_out(3, 0x03); // 8 bits, no parity, one stop bit
+  uart16550_out(2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
 }
 
 int query_uart16550(uintptr_t fdt)
